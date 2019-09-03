b19f2728db1ab2d9ac728019339ea973089fba35
drbarq <J.Tustin@gmail.com>
2019-09-03 15:30:59 -0600

    adding counter state

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -7,13 +7,16 @@ const ImageScreen = () => {
         <View>
             <ImageDetail 
                 title="Forest" 
-                imageSource={require('../../assets/images/forest.jpg')} />
+                imageSource={require('../../assets/images/forest.jpg')} 
+                imageScore={9} />
             <ImageDetail 
                 title="Beach" 
-                imageSource={require('../../assets/images/beach.jpg')}/>
+                imageSource={require('../../assets/images/beach.jpg')}
+                imageScore={7} />
             <ImageDetail 
                 title="Mountain" 
-                imageSource={require('../../assets/images/mountain.jpg')}/>
+                imageSource={require('../../assets/images/mountain.jpg')}
+                imageScore={10} />
         </View>
     )
 }
