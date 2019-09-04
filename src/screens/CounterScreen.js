1f71ad0422cca15da3d06899566f146d3d016dcd
drbarq <J.Tustin@gmail.com>
2019-09-04 16:05:51 -0600

    refactored with reducer counter

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,22 +1,38 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
+// useReducer example
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increase':
+            return { ...state, counter: state.counter + action.payload }
+        case 'decrease':
+            return { ...state, counter: state.counter + action.payload }
+        default:
+            return state
+    }
+}
+
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0)
+    const [state, dispatch] = useReducer(reducer, { counter: 0 })
+
+    console.log(state)
 
     return (
         <View>
-            <Text>Current Count: {counter} </Text>
+            <Text>Current Count: {state.counter} </Text>
             <Button 
                 title="Increase" 
                 onPress={() => 
-                    setCounter(counter + 1)
+                    dispatch({ type: 'increase', payload: 1 })
                 }
             />
             <Button 
                 title="Decrease" 
                 onPress={() => 
-                    setCounter(counter - 1)
+                    dispatch({ type: 'decrease', payload: -1 })
                 }        
             />
         </View>
@@ -26,3 +42,38 @@ const CounterScreen = () => {
 const styles = StyleSheet.create({})
 
 export default CounterScreen
+
+
+
+
+// useState example
+
+// import React, { useState } from 'react'
+// import { View, Text, StyleSheet, Button } from 'react-native'
+
+
+// const CounterScreen = () => {
+//     const [counter, setCounter] = useState(0)
+
+//     return (
+//         <View>
+//             <Text>Current Count: {counter} </Text>
+//             <Button 
+//                 title="Increase" 
+//                 onPress={() => 
+//                     setCounter(counter + 1)
+//                 }
+//             />
+//             <Button 
+//                 title="Decrease" 
+//                 onPress={() => 
+//                     setCounter(counter - 1)
+//                 }        
+//             />
+//         </View>
+//     )
+// }
+
+// const styles = StyleSheet.create({})
+
+// export default CounterScreen
