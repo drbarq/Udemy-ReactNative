802300910b564b5bb4432df7c8d970a63cfdca2d
drbarq <J.Tustin@gmail.com>
2019-09-04 12:05:12 -0600

    added in new app code

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -9,11 +9,24 @@ const SquareScreen = () => {
     const [green, setGreen] = useState(0)
     const [blue, setBlue] = useState(0)
 
+    const setColor = (color, change) => {
+        // color is red green or blue 
+        // change would be the color increment +15 || -15
+        switch (color) {
+            case 'red':
+                red + change > 255 || red + change < 0 ? null : setRed(red + change)
+            case 'blue':
+                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
+            case 'green':
+                green + change > 255 || green + change < 0 ? null : setGreen(green +  change)
+        } 
+    }
+
     return (
         <View>
             <ColorCounter 
-                onIncrease={() => setRed(red + COLOR_INCREMENT)} 
-                onDecrease={() => setRed(red - COLOR_INCREMENT)}
+                onIncrease={() => setColor('red', COLOR_INCREMENT)}
+                onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
                 color="Red">
             </ColorCounter>
             <ColorCounter 
