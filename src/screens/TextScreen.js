22a2a885bd2346184d9f8d02f2a2d440b3a8b72e
drbarq <J.Tustin@gmail.com>
2019-09-04 16:52:48 -0600

    working password example

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,20 +2,20 @@ import React, { useState } from 'react'
 import { View, Text, StyleSheet, TextInput } from 'react-native'
 
 const TextScreen = () => {
-    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
 
     return (
         <View>
-            <Text>Enter Name:</Text>
+            <Text style={styles.header}>Enter Password:</Text>
             <TextInput 
             style={styles.input}
             autoCapitalize="none"
             autoCorrect={false}
-            value={name}
-            onChangeText={(newValue) => setName(newValue)}
+            value={password}
+            onChangeText={(newPassword) => setPassword(newPassword)}
             >
             </TextInput>
-            <Text>My name is {name} </Text>
+            {password.length > 4 ? null : <Text>Please enter a password longer than 5 characters</Text>}
         </View>
     )
 }
@@ -25,6 +25,9 @@ const styles = StyleSheet.create({
         margin: 15,
         borderColor: 'black',
         borderWidth: 1
+    },
+    header: {
+        fontSize: 25
     }
 })
 
